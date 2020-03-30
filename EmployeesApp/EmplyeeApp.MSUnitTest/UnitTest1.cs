c670a8d7fa69d0a834d3fcfb7a0ece9d1345c2c2
Harsha Vardhan <harsha.kv@outlook.com>
2020-03-30 20:59:17 +0530

    second commit

---
+++ EmployeesApp/EmplyeeApp.MSUnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeesApp.Validation;
+
+namespace Employee.MSUnitTest
+{
+    [TestClass]
+    public class AccountNumberValidationTests
+    {
+        private readonly AccountNumberValidation _validation;
+        public AccountNumberValidationTests()
+        {
+            _validation = new AccountNumberValidation();
+        }
+        [TestMethod]
+        public void IsValid_ValidAccountNumber_ReturnsTrue()
+        {
+            Assert.IsTrue(_validation.IsValid("123-4543234576-23"));
+        }
+
+    }
+}
