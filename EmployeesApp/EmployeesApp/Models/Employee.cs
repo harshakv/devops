f63fb17709f0142f06b43b4ac52c2445e32930d8
Harsha Vardhan <harsha.kv@outlook.com>
2020-03-31 02:13:50 +0530

    fix commit

--- EmployeesApp/EmployeesApp/Models/Employee.cs
+++ EmployeesApp/EmployeesApp/Models/Employee.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace EmployeesApp.Models
 {
     [Table("Employee")]
-    public class Empl oyee
+    public class Employee
     {
         public Guid Id { get; set; }
 
